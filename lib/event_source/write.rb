f666313dcc4ca92a74d5a54cd8c208282fe7da32
Scott Bellware <sbellware@gmail.com>
2017-01-10 17:36:52 -0600

    Specialized write code is removed from the generalization

--- lib/event_source/write.rb
+++ lib/event_source/write.rb
@@ -8,8 +8,6 @@ module EventSource
         cls.extend Call
         cls.extend Configure
 
-        dependency :put
-
         abstract :configure
       end
     end
@@ -42,45 +40,13 @@ module EventSource
       logger.trace(tags: [:data, :event_data]) { event_data.pretty_inspect }
 
       batch = Array(event_data)
-      position = write_batch(batch, stream_name, expected_version: expected_version)
+
+      position = write(batch, stream_name, expected_version: expected_version)
 
       logger.info { "Wrote event data (Stream Name: #{stream_name}, Expected Version: #{expected_version.inspect})" }
       logger.info(tags: [:data, :event_data]) { event_data.pretty_inspect }
 
       position
-    end
-
-    def write_batch(batch, stream_name, expected_version: nil)
-      logger.trace { "Writing batch (Stream Name: #{stream_name}, Number of Events: #{batch.length}, Expected Version: #{expected_version.inspect})" }
-
-      unless expected_version.nil?
-        expected_version = ExpectedVersion.canonize(expected_version)
-      end
-
-      last_position = nil
-      put.session.transaction do
-        batch.each do |event_data|
-          last_position = write(event_data, stream_name, expected_version: expected_version)
-
-          unless expected_version.nil?
-            expected_version += 1
-          end
-        end
-      end
-
-      logger.debug { "Wrote batch (Stream Name: #{stream_name}, Number of Events: #{batch.length}, Expected Version: #{expected_version.inspect})" }
-
-      last_position
-    end
-
-    def write(event_data, stream_name, expected_version: nil)
-      logger.trace { "Writing event data (Stream Name: #{stream_name}, Type: #{event_data.type}, Expected Version: #{expected_version.inspect})" }
-      logger.trace(tags: [:data, :event_data]) { event_data.pretty_inspect }
-
-      put.(event_data, stream_name, expected_version: expected_version).tap do
-        logger.debug { "Wrote event data (Stream Name: #{stream_name}, Type: #{event_data.type}, Expected Version: #{expected_version.inspect})" }
-        logger.debug(tags: [:data, :event_data]) { event_data.pretty_inspect }
-      end
     end
   end
 end
