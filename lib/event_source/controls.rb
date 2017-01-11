ac46c7ac2fe4d56e6ace77dd075a3f82158f5e82
Scott Bellware <sbellware@gmail.com>
2017-01-11 17:12:45 -0600

    Missing IDs are assigned when writing

--- lib/event_source/controls.rb
+++ lib/event_source/controls.rb
@@ -1,7 +1,10 @@
 require 'securerandom'
 
+require 'identifier/uuid/controls'
+
 require 'event_source/controls/random_value'
 require 'event_source/controls/time'
+require 'event_source/controls/id'
 require 'event_source/controls/category'
 require 'event_source/controls/stream_name'
 require 'event_source/controls/stream'
@@ -11,3 +14,4 @@ require 'event_source/controls/event_data/hash'
 require 'event_source/controls/event_data/metadata'
 require 'event_source/controls/event_data/write'
 require 'event_source/controls/event_data/read'
+require 'event_source/controls/write'
