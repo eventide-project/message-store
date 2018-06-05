e3623b9c2ac51013ad5a225c03f54c0e09210a8a
Scott Bellware <sbellware@gmail.com>
2018-06-05 10:45:03 -0500

    Duplicate write error is removed. It's not a state that can be actuated.

--- lib/message_store/write.rb
+++ lib/message_store/write.rb
@@ -15,11 +15,6 @@ module MessageStore
       end
     end
 
-    class Error < RuntimeError
-      class Duplicate < Error
-      end
-    end
-
     module Build
       def build(session: nil)
         instance = new
