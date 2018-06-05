102885e7f0a9e4a9f80f5c4282b6bdde4d3d952e
Scott Bellware <sbellware@gmail.com>
2018-06-05 09:52:54 -0500

    Optionally omit ID from stream name

--- lib/message_store/controls/stream_name.rb
+++ lib/message_store/controls/stream_name.rb
@@ -3,19 +3,24 @@ module MessageStore
     module StreamName
       def self.example(category: nil, id: nil, type: nil, types: nil, randomize_category: nil)
         category ||= Category.example(category: category, randomize_category: randomize_category)
-        id ||= Identifier::UUID.random
+
+        if id == :none
+          id = nil
+        else
+          id ||= Identifier::UUID.random
+        end
 
         stream_name(category, id, type: type, types: types)
       end
 
-      def self.stream_name(category_name, id=nil, type: nil, types: nil)
+      def self.stream_name(category, id=nil, type: nil, types: nil)
         types = Array(types)
         types.unshift(type) unless type.nil?
 
         type_list = nil
         type_list = types.join('+') unless types.empty?
 
-        stream_name = category_name
+        stream_name = category
         stream_name = "#{stream_name}:#{type_list}" unless type_list.nil?
         stream_name = "#{stream_name}-#{id}" unless id.nil?
 
