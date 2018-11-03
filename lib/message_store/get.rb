efd0f87df9ef75eb9b3138befa735c8a08c4d99e
Scott Bellware <sbellware@gmail.com>
2018-11-03 18:22:57 -0500

    Log tags are standardized

--- lib/message_store/get.rb
+++ lib/message_store/get.rb
@@ -34,11 +34,11 @@ module MessageStore
       def call(stream_name=nil, position: nil)
         position ||= 0
 
-        logger.trace(tag: :control) { "Getting (Position: #{position}, Batch Size: #{batch_size}, Stream Name: #{stream_name.inspect})" }
+        logger.trace(tag: :get) { "Getting (Position: #{position}, Batch Size: #{batch_size}, Stream Name: #{stream_name.inspect})" }
 
-        logger.debug(tags: [:control, :data]) { "Items: \n#{items.pretty_inspect}" }
-        logger.debug(tag: :control) { "Position: #{position.inspect}" }
-        logger.debug(tag: :control) { "Batch Size: #{batch_size.inspect}" }
+        logger.debug(tag: :data) { "Items: \n#{items.pretty_inspect}" }
+        logger.debug(tag: :data) { "Position: #{position.inspect}" }
+        logger.debug(tag: :data) { "Batch Size: #{batch_size.inspect}" }
 
         unless self.class.category?(stream_name)
           index = (items.index { |i| i.position >= position })
@@ -46,19 +46,19 @@ module MessageStore
           index = (items.index { |i| i.global_position >= position })
         end
 
-        logger.debug(tag: :control) { "Index: #{index.inspect}" }
+        logger.debug(tag: :data) { "Index: #{index.inspect}" }
 
         if index.nil?
           items = []
         else
           range = index..(index + batch_size - 1)
-          logger.debug(tag: :control) { "Range: #{range.pretty_inspect}" }
+          logger.debug(tag: :data) { "Range: #{range.pretty_inspect}" }
 
           items = self.items[range]
         end
 
-        logger.info(tags: [:control, :data]) { "Got: \n#{items.pretty_inspect}" }
-        logger.info(tag: :control) { "Finished getting (Position: #{position}, Stream Name: #{stream_name.inspect})" }
+        logger.info(tag: :data) { "Got: \n#{items.pretty_inspect}" }
+        logger.info(tag: :get) { "Finished getting (Position: #{position}, Stream Name: #{stream_name.inspect})" }
 
         items
       end
