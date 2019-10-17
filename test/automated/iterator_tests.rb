1b13d2435524524ba9d9a58b4260b7f20fb9b51f
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-17 16:16:20 -0500

    Automated test runner supplies exclude file pattern directly into CLI

--- test/automated/iterator_tests.rb
+++ test/automated/iterator_tests.rb
@@ -1,5 +1,6 @@
-ENV['TEST_BENCH_EXCLUDE_PATTERN'] ||= '/_|sketch|(_init\.rb|_tests\.rb)\z'
-
 require_relative './automated_init'
 
-TestBench::CLI::Run.('test/automated/iterator')
+TestBench::CLI::Run.(
+  'test/automated/iterator',
+  exclude_file_pattern: %r{/_|sketch|(_init\.rb|_tests\.rb)\z}
+)
