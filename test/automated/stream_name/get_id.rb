eda89378dbf95856595aae63694135671b3a2d6f
Scott Bellware <sbellware@gmail.com>
2019-11-01 13:32:21 -0500

    Stream name tests are clarified

--- test/automated/stream_name/get_id.rb
+++ test/automated/stream_name/get_id.rb
@@ -2,21 +2,21 @@ require_relative '../automated_init'
 
 context "Stream Name" do
   context "Get ID" do
-    context "Single ID" do
+    context "Single ID in the Stream Name" do
       id = 'some_id'
       stream_name = "someCategory-#{id}"
 
       stream_id = StreamName.get_id(stream_name)
 
-      test "Array with single ID" do
+      test "ID value is parsed" do
         assert(stream_id == 'some_id')
       end
     end
 
-    context "No ID" do
+    context "No ID in the Stream Name" do
       stream_id = StreamName.get_id('someStream')
 
-      test "Nil" do
+      test "ID is nil" do
         assert(stream_id.nil?)
       end
     end
--- test/automated/stream_name/get_ids.rb
+++ test/automated/stream_name/get_ids.rb
@@ -2,32 +2,32 @@ require_relative '../automated_init'
 
 context "Stream Name" do
   context "Get IDs" do
-    context "Compound ID" do
+    context "Compound ID in the Stream Name" do
       id = 'some_id+some_other_id'
       stream_name = "someCategory-#{id}"
 
       stream_ids = StreamName.get_ids(stream_name)
 
-      test "Array of compound IDs" do
+      test "ID is a list of values" do
         assert(stream_ids == ['some_id', 'some_other_id'])
       end
     end
 
-    context "Single ID" do
+    context "Single ID in the Stream Name" do
       id = 'some_id'
       stream_name = "someCategory-#{id}"
 
       stream_ids = StreamName.get_ids(stream_name)
 
-      test "Array with single ID" do
+      test "ID list has a single entry" do
         assert(stream_ids == ['some_id'])
       end
     end
 
-    context "No ID" do
+    context "No ID in the Stream Name" do
       stream_ids = StreamName.get_ids('someStream')
 
-      test "Empty array" do
+      test "ID is an empty list" do
         assert(stream_ids == [])
       end
     end
