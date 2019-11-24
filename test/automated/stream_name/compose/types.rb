5cdebfcdbd45447a781115f234a13168abc04df8
Scott Bellware <sbellware@gmail.com>
2019-11-23 20:54:24 -0600

    Type permutations

--- test/automated/stream_name/compose/types.rb
+++ test/automated/stream_name/compose/types.rb
@@ -3,27 +3,55 @@ require_relative '../../automated_init'
 context "Stream Name" do
   context "Compose" do
     context "Types" do
-      context "Category and Type" do
-        stream_name = StreamName.stream_name('someCategory', type: 'someType')
+      context "Singular Types" do
+        context "Category and Type" do
+          stream_name = StreamName.stream_name('someCategory', type: 'someType')
 
-        test "Stream name is the category and the Type" do
-          assert(stream_name == 'someCategory:someType')
+          test "Stream name is the category and the Type" do
+            assert(stream_name == 'someCategory:someType')
+          end
+        end
+
+        context "Category and Types" do
+          stream_name = StreamName.stream_name('someCategory', types: 'someTypes')
+
+          test "Stream name is the category and the types" do
+            assert(stream_name == 'someCategory:someTypes')
+          end
+        end
+
+        context "Category, Type, and Types" do
+          stream_name = StreamName.stream_name('someCategory', type: 'someType', types: 'someTypes')
+
+          test "Stream name is the category and the types" do
+            assert(stream_name == 'someCategory:someType+someTypes')
+          end
         end
       end
 
-      context "Category and Types" do
-        stream_name = StreamName.stream_name('someCategory', types: ['someType', 'someOtherType'])
+      context "Plural Types" do
+        context "Category and Type" do
+          stream_name = StreamName.stream_name('someCategory', type: ['someType', 'someOtherType'])
 
-        test "Stream name is the category and the types delimited by the plus (+) sign" do
-          assert(stream_name == 'someCategory:someType+someOtherType')
+          test "Stream name is the category and the type" do
+            assert(stream_name == 'someCategory:someType+someOtherType')
+          end
         end
-      end
 
-      context "Category, Type, and Types" do
-        stream_name = StreamName.stream_name('someCategory', type: 'someType', types: ['someOtherType', 'yetAnotherYet'])
+        context "Category and Types" do
+          stream_name = StreamName.stream_name('someCategory', types: ['someType', 'someOtherType'])
 
-        test "Stream name is the category and the types delimited by the plus (+) sign" do
-          assert(stream_name == 'someCategory:someType+someOtherType+yetAnotherYet')
+          test "Stream name is the category and the types" do
+            assert(stream_name == 'someCategory:someType+someOtherType')
+          end
+        end
+
+        context "Category, Type, and Types" do
+          stream_name = StreamName.stream_name('someCategory', type: ['someType', 'someOtherType'], types: ['yetAnotherType', 'andAnotherType'])
+
+          test "Stream name is the category and the types delimited by the plus (+) sign" do
+            assert(stream_name == 'someCategory:someType+someOtherType+yetAnotherType+andAnotherType')
+          end
         end
       end
     end
