e808359cefd7f8751e5886c0612c76c56390623d
Scott Bellware <sbellware@gmail.com>
2019-12-01 10:43:54 -0600

    Category control receives category type and types

--- lib/message_store/controls/category.rb
+++ lib/message_store/controls/category.rb
@@ -1,7 +1,7 @@
 module MessageStore
   module Controls
     module Category
-      def self.example(category: nil, randomize_category: nil)
+      def self.example(category: nil, type: nil, types: nil, randomize_category: nil)
         if randomize_category.nil?
           if !category.nil?
             randomize_category = false
@@ -15,6 +15,8 @@ module MessageStore
         if randomize_category
           category = "#{category}#{SecureRandom.hex(16)}XX"
         end
+
+        category = Controls::StreamName.stream_name(category, type: type, types: types)
 
         category
       end
