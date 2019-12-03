53b3bcf5c2032b1e151fac99908ccb08a1b63f61
Scott Bellware <sbellware@gmail.com>
2019-12-03 11:15:01 -0600

    Category retrieval errors are moved to the platform-specific implementation

--- lib/message_store/get/consumer_group.rb
+++
@@ -1,7 +0,0 @@
-module MessageStore
-  module Get
-    module ConsumerGroup
-      Error = Class.new(RuntimeError)
-    end
-  end
-end
--- lib/message_store/get/correlation.rb
+++
@@ -1,7 +0,0 @@
-module MessageStore
-  module Get
-    module Correlation
-      Error = Class.new(RuntimeError)
-    end
-  end
-end
